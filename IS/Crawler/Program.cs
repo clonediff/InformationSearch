01b0fbc2af7103da77919ce1a7adbd54be0affea
clonediff <32036592+clonediff@users.noreply.github.com>
2025-03-27 23:08:32 +0300

    [HW1] add Crawler

---
+++ IS/Crawler/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Net.Mime;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+const string pages = "pages";
+const string indexTxt = "index.txt";
+HashSet<string> ignoreTags = ["script", "style", "head", "meta", "link"];
+
+var cts = new CancellationTokenSource();
+
+string[] fileLinks = ["https://tatmedia.ru"];//await File.ReadAllLinesAsync(indexTxt, cts.Token);
+
+var links = new Queue<Uri>(fileLinks.Select(x => new Uri(x)));
+var usedLinks = new HashSet<string>(fileLinks);
+List<string> resPages = [];
+
+if (!Directory.Exists(pages)) Directory.CreateDirectory(pages);
+
+while (links.Count != 0 && resPages.Count < 100) {
+    var link = links.Dequeue();
+    var html = await GetHtml(link, cts.Token);
+    var doc = new HtmlDocument();
+    doc.LoadHtml(html);
+
+    foreach (var anchor in GetAnchors(doc.DocumentNode, link))
+    {
+        if (!usedLinks.Add(anchor.ToString())) continue;
+        Console.WriteLine(anchor);
+        links.Enqueue(anchor);
+    }
+
+    var text = ExtractReadableText(doc.DocumentNode);
+    var textWords = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    if (textWords.Length < 1000) continue;
+    
+    resPages.Add(link.ToString());
+    await File.WriteAllTextAsync(Path.Combine(pages, $"{resPages.Count}.txt"), text, cts.Token);
+    Console.WriteLine($"{link.ToString()} saved ({resPages.Count})");
+}
+
+await File.WriteAllTextAsync(indexTxt, string.Join('\n', resPages.Select((link, i) => $"{i + 1} - {link}")), cts.Token);
+
+return;
+
+async Task<string> GetHtml(Uri url, CancellationToken ct)
+{
+    try
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url, ct);
+
+        return response.Content.Headers.ContentType?.MediaType == MediaTypeNames.Text.Html
+            ? await response.Content.ReadAsStringAsync(ct)
+            : string.Empty;
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"Cannot get HTML from {url}, {e.HttpRequestError}, {e.Message}");
+        return string.Empty;
+    }
+}
+
+string ExtractReadableText(HtmlNode html)
+{
+    switch (html.NodeType)
+    {
+        case HtmlNodeType.Comment:
+            return string.Empty;
+        case HtmlNodeType.Text:
+            return html.InnerText.Trim() + '\n';
+        case HtmlNodeType.Document:
+        case HtmlNodeType.Element:
+        default:
+        {
+            return ignoreTags.Contains(html.Name)
+                ? string.Empty
+                : string.Join(' ', html.ChildNodes.Select(ExtractReadableText).Where(x => !string.IsNullOrWhiteSpace(x)));
+        }
+    }
+}
+
+IEnumerable<Uri> GetAnchors(HtmlNode html, Uri baseUri)
+{
+    return html.Descendants("a")
+        .Where(x => x.Attributes["href"] != null)
+        .Select(x => x.Attributes["href"].Value)
+        .Where(x => WebsiteRegex().IsMatch(x))
+        .Select(x => new Uri(baseUri, x));
+}
+
+partial class Program
+{
+    [GeneratedRegex(@"^(https?:\/\/[\w\-\.]+(\.[a-z]{2,})?(:\d+)?(\/[\w\-\.]*)*|(\.?\.?\/)?[\w\-.]+(\/[\w\-.]*)*)$")]
+    private static partial Regex WebsiteRegex();
+}
